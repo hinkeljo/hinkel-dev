5fa46d072a6141d9214b145598c26f5bb320e03c
Jonathan Hinkel <hinkeljonathan@protonmail.com>
2024-07-25 22:36:48 +0200

    improved SEO metrics in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,17 +10,26 @@ export default function Footer() {
         </p>
       </aside>
       <nav>
-        <h6 className="footer-title">Links</h6>
+        <p className="text-lg">Additional links</p>
         <div className="grid grid-flow-col gap-4">
-          <a href="mailto:jonathan@hinkel.dev" target="_blank">
+          <a
+            href="mailto:jonathan@hinkel.dev"
+            target="_blank"
+            aria-label="Write me an email"
+          >
             <Icon icon="mdi:email-outline" className="w-8 h-8" />
           </a>
-          <a href="https://github.com/hinkeljo" target="_blank">
+          <a
+            href="https://github.com/hinkeljo"
+            target="_blank"
+            aria-label="My GitHub page"
+          >
             <Icon icon="mdi:github" className="w-8 h-8" />
           </a>
           <a
             href="https://de.linkedin.com/in/jonathan-hinkel-43a640215"
             target="_blank"
+            aria-label="My LinkedIn page"
           >
             <Icon icon="mdi:linkedin" className="w-8 h-8" />
           </a>
